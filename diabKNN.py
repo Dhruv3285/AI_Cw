12cc26d3e20c5baa93061f36bfec248d313ae034
Dhruv3285 <36102225+Dhruv3285@users.noreply.github.com>
2021-12-14 21:22:48 +0000

    added more analysers
    added more analysers

--- diabKNN.py
+++ diabKNN.py
@@ -14,24 +14,21 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.model_selection import cross_val_score
+import seaborn as sns
+from pylab import rcParams
+import matplotlib.pyplot as plt 
+
 
 
 
 data = pd.read_csv('diabetes_data_upload.csv')
 
 
-print(data.columns)
-
 data['Gender'] = data['Gender'].replace("Male",1).replace("Female",0)
 
-#data['Polyuria'] = data['Polyuria'].replace("Yes",1).replace("No",0)
-
-#data['Polydipsia'] = data['Polydipsia'].replace("Yes",1).replace("No",0)
-
-#data['muscle stiffness'] = data['muscle stiffness'].replace("Yes",1).replace("No",0)
-#data['Alopecia'] = data['Alopecia'].replace("Yes",1).replace("No",0)
 data = data.replace("Yes",1).replace("No",0)
-data['class'] = data['class'].replace("Positive",1).replace("Negative",0)
+data['class'] = data['class'].replace("Positive",1).replace("Negative",0) 
 
 
 # split dataset
@@ -70,10 +67,12 @@ print(accuracy_score(y_test, y_pred))
 disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=classifier.classes_)
 disp.plot()
 
-print("<-----------------------manhattan metric------------------------------->")
+print("<-----------------------manhattan metric with all the features------------------------------->")
+
+
 
 classifier2 = KNeighborsClassifier(n_neighbors= 9, p=2, metric = 'manhattan')
-classifier2.fit(X_train, y_train)
+classifier2.fit(X_train, y_train) 
 KNeighborsClassifier(algorithm='auto', leaf_size=30, metric='manhattan',
                      metric_params=None, n_jobs=1, n_neighbors=9, p=2,
                      weights='uniform')
@@ -86,13 +85,85 @@ print (cm2)
 print(f1_score(y_test, y_pred2))
 print(accuracy_score(y_test, y_pred2))
 
+# PCA(princple componenet analysis) 
+def plot_correlation(datab):
+    rcParams['figure.figsize']=15,20
+    fig= plt.figure()
+    sns.heatmap(datab.corr(), annot=True, fmt= ".2f")
+    plt.show()
+    fig.savefig('corr.png')
+
+plot_correlation(data)
+
+
+k_scores = []
+
+print("<-----------------------manhattan metric but age dropped out----------------->")
+# dropping out gender
+data.drop('Gender',1,inplace=True) 
+ # split dataset
+A = data.iloc[:, 0:14]
+b = data.iloc[:, 14]
+A_train, A_test, b_train, b_test = train_test_split(A, b, random_state=0, test_size= 0.2)
+
+# Feature scaling
+sc_A = StandardScaler()
+A_train = sc_A.fit_transform(A_train)
+
+classifier3 = KNeighborsClassifier(n_neighbors= 9, p=2, metric = 'manhattan')
+classifier3.fit(A_train, b_train)
+KNeighborsClassifier(algorithm='auto', leaf_size=30, metric='manhattan',
+     metric_params=None, n_jobs=1, n_neighbors=9, p=2,
+     weights='uniform')
+
+b_pred = classifier3.predict(A_test)
+
+# Evaluate model using matrix 
+cm3 = confusion_matrix(b_test, b_pred)
+print (cm3)
+print(f1_score(b_test, b_pred))
+print(accuracy_score(b_test, b_pred))
 
 
 
 
 
 
+def knnManhattan(kval, dataset):
+      print("<-----------------------manhattan metric but age dropped out of it k="+str(kval)+"----------------->")
+  
+  
+      # split dataset
+      A = dataset.iloc[:, 0:14]
+      b = dataset.iloc[:, 14]
+      A_train, A_test, b_train, b_test = train_test_split(A, b, random_state=0, test_size= 0.2)
+
+      # Feature scaling
+      sc_A = StandardScaler()
+      A_train = sc_A.fit_transform(A_train)
+
+      classifier = KNeighborsClassifier(n_neighbors= kval, p=2, metric = 'manhattan')
+      classifier.fit(A_train, b_train)
+      KNeighborsClassifier(algorithm='auto', leaf_size=30, metric='manhattan',
+                     metric_params=None, n_jobs=1, n_neighbors=kval, p=2,
+                     weights='uniform')
+
+      b_pred = classifier.predict(A_test)
+
+    # Evaluate model using cross validation
+      scores = cross_val_score(classifier, A, b, cv=5, scoring='accuracy')
+      k_scores.append(scores.mean())
 
 
+k_range = range(1, 31)
+# use iteration to caclulator different k in models, then return the average accuracy based on the cross validation
+for k in k_range:
+    knn = knnManhattan(k,data)
+
+# plot to see clearly
+plt.plot(k_range, k_scores)
+plt.xlabel('Value of K for KNN')
+plt.ylabel('Cross-Validated Accuracy')
+plt.show()
 
 
